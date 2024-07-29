a78fbf4aeb5a7840227ec7fe296444797a610ed2
debnathsubhankar <subhankardebnat923262@gmail.com>
2024-07-29 16:39:28 +0530

    third comment

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from '../Navbar';
-import banar from "../../Assates/baner2.jpg"
+import banar from "../../Assates/baner.jpg"
+import thumb from "../../Assates/thumb.png";
+import play from "../../Assates/play.png";
+import info from "../../Assates/info.png";
 
 const Home = () => {
   return (
@@ -9,8 +12,14 @@ const Home = () => {
         <div className="hero">
             <img src={banar} alt="" className='hero_banar'/>
             <div className="hero_caption">
-                <img src="" alt="" className='caption_img'/>
-                <p className='caption_title'></p>
+                <img src={thumb} alt="" className='caption_img'/>
+                <p className='caption_title'>
+                Stranger Things is an American science fiction drama television series created by the Duffer Brothers for Netflix
+                </p>
+                <div className="hero_btn">
+                  <button className='btn'><img src={play} alt="" />Play</button>
+                  <button className='btn btn-dr'><img src={info} alt="" /> More Info</button>
+                </div>
             </div>
         </div>
     </div>
