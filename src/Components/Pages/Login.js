944503d5305138a86f89d4f77a8716931c57dccb
debnathsubhankar <subhankardebnat923262@gmail.com>
2024-08-01 20:13:37 +0530

    craete login page with switch function

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../Assates/logo.png";
 
 const Login = () => {
+  const [prefarence, setPrefarence] = useState("Login");
   return (
-    <div>
+    <div className="main_box">
       <div className="head">
         <img src={logo} alt="logo" />
       </div>
       <div className="log_box">
         <div className="card">
-          <h3>Sign Up</h3>
-          <input type="text" placeholder="Name" />
+          <h3>{prefarence}</h3>
+          {prefarence !== "Login" ? (
+            <input type="text" placeholder="Name" />
+          ) : (
+            <></>
+          )}
           <input type="text" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <span>
             <input type="checkbox" />
             <p>Remamber me.</p>
           </span>
-          <button>Sign Up</button>
+          <button>{prefarence}</button>
         </div>
       </div>
       <div className="switch_pg">
-        <span>
-          <p>If you are already have an account</p>{" "}
-          <p className="click">Sign In</p>
-        </span>
-        <span>
-          <p>New in Netflix</p> <p className="click">Sign Up</p>
-        </span>
+        {prefarence !== "Login" ? (
+          <span>
+            <p>If you already have an account</p>
+            <p className="click" onClick={() => setPrefarence("Login")}>
+              Sign In
+            </p>
+          </span>
+        ) : (
+          <span>
+            <p>New in Netflix</p>
+            <p className="click" onClick={() => setPrefarence("Sign Up")}>
+              Sign Up
+            </p>
+          </span>
+        )}
       </div>
     </div>
   );
